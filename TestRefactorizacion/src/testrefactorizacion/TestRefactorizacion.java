7b5a0b9c0b2d460485245d8caefc2869661c316d
JorgeDAW <35879413+JorgeDAW@users.noreply.github.com>
2018-01-28 00:00:49 +0100

    Inicio das Probas

--- TestRefactorizacion/src/testrefactorizacion/TestRefactorizacion.java
+++ TestRefactorizacion/src/testrefactorizacion/TestRefactorizacion.java
@@ -16,7 +16,7 @@ public class TestRefactorizacion {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        cliente c=new cliente("IES de Rodeira");
+        cliente c=new cliente("IES de Rodeira"); 
     }
     
 }
